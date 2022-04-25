cb1814447e5d1b163f0d2bc72863419ce945005f
Lucas White Rossi <lucaswrossi@gmail.com>
2022-04-25 18:25:07 -0300

    estilização inicial

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,20 +1,19 @@
 import { Link } from "react-router-dom";
 import './style.modules.css'
-import logoImg from "./../../assets/img/logo.png"
+import logoImg from "./../../assets/img/logo-nome.png"
 export function Navbar() {
 return (
-    <nav className="navbar navbar-dark bg-dark">
+    <nav className="navbar navbar-dark bg-warning">
     <h1>
     <Link to="/" style={{ textDecoration: "none" }}>
-      <div style={{color:"white"}}>
-        <img src={logoImg} alt="Fábrica de Personas" style={{width:"100px",height:"auto"}}/>
-        Home
+      <div style={{color:"#713e9b"}}>
+        <img src={logoImg} alt="Fábrica de Personas" style={{width:"400px",height:"auto"}}/>
       </div>
       </Link>
     </h1>
     <div className="nav-items">
-        <Link className="btn btn-outline-success m-2" to="/cadastro">CADASTRO</Link>
-        <Link className="btn btn-outline-success m-2" to="/personas">PEDIDOS</Link>
+        <Link className="btn btn-outline-danger m-2" to="/cadastro">CRIAR PERSONA</Link>
+        <Link className="btn btn-outline-danger m-2" to="/personas">PERSONAS</Link>
     </div>
   </nav>
 );
